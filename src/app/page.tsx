a805743450fae3fb1ea86006732a1ce59d2075a2
Mark Dacurawat <markmarkifffff@gmail.com>
2024-05-08 18:55:24 +0800

    fix: Fixed Build Errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,15 @@
 "use client";
-import Image from "next/image";
+import { NextPage } from "next";
 import Sidebar from "../components/sidebar/SideBar";
 import ChatBotContent from "@/components/content/ChatBotContent";
 import GenerateImage from "@/components/content/GenerateImage";
 import MumakerContent from "@/components/content/MumakerContent";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faMessage,
-  faImage,
-  faPaperPlane,
-} from "@fortawesome/free-solid-svg-icons";
-import { FormEvent, useState, useRef, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
-import axios from "axios";
 
-export const RenderContent = () => {
+const RenderContent: React.FC = () => {
   const searchParams = useSearchParams();
   const tab = searchParams.get("tab");
-  if (tab !== "chatbot" && tab !== "generate-image" && tab !== "mumaker") {
-    return (
-      <div className="w-full h-full flex flex-col items-center justify-center">
-        <h1 className="text-xl text-foreground">404 not found</h1>
-      </div>
-    );
-  }
   switch (tab) {
     case null:
       return <ChatBotContent />;
@@ -34,19 +19,23 @@ export const RenderContent = () => {
       return <GenerateImage />;
     case "mumaker":
       return <MumakerContent />;
+    default:
+      return (
+        <div className="w-full h-full flex flex-col items-center justify-center">
+          <h1 className="text-xl text-foreground">404 not found</h1>
+        </div>
+      );
   }
 };
 
-export default function Home() {
-  // Define function to render content based on route
-
+const Home: NextPage = () => {
   return (
     <main className="dark w-100 h-screen bg-background text-foreground flex flex-col justify-center items-center">
       <Sidebar />
       <div className="chatBotContainer w-full h-full flex rounded-lg">
         <div className="w-[250px] h-full max-[910px]:hidden"></div>
         <div className="flex-1 bg-pallete-yellow" id="content">
-          <Suspense>
+          <Suspense fallback={<div>Loading...</div>}>
             <RenderContent />
           </Suspense>
         </div>
@@ -55,4 +44,6 @@ export default function Home() {
       {/* Image by <a href="https://www.freepik.com/free-vector/technology-background-concept_7328095.htm#query=red%20website%20background&position=4&from_view=keyword&track=ais&uuid=3cbe4071-56bd-401a-8e70-c112bc68d861">Freepik</a> */}
     </main>
   );
-}
+};
+
+export default Home;
